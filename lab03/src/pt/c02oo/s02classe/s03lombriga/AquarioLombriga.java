e81af44a6347fc3c84557802fa9db0d0e18bcd9a
brabesf <84363153+brabesf@users.noreply.github.com>
2022-03-31 18:24:12 -0300

    Começando lab03
    Template, classe AquarioLombriga funfando "em teoria" falta: Condicionar a mensagem de erro para posicao invalida, salvar animacao em array de strings, usar o tk para salvar animacao, verificar se ta tudo suave

---
+++ lab03/src/pt/c02oo/s02classe/s03lombriga/AquarioLombriga.java
@@ -0,0 +1,98 @@
+package pt.c02oo.s02classe.s03lombriga;
+
+public class AquarioLombriga{
+    int sizeAq;
+    int sizeLo;
+    int headPos;
+    int toLeft;
+   
+    AquarioLombriga(int sizeAq, int sizeLo, int headPos){
+        this.sizeAq = sizeAq;
+        this.sizeLo = sizeLo;
+        this.headPos = headPos;
+        this.toLeft = 1;
+    }
+   
+    public void crescer(){
+        if(toLeft == 1){
+            if(sizeAq >= (sizeLo + headPos - 1)){
+                return;
+            }else{
+                sizeLo++;
+                return;
+            }
+        }else{
+            if(headPos == sizeAq){
+                return;
+            }else{
+                sizeLo++;
+                return;
+            }
+        }
+    }
+   
+    public void virar(){
+        if(toLeft == 1){
+            if(headPos == 1){
+                toLeft = 0;
+                headPos = sizeLo;
+                return;
+            }
+            toLeft = 0;
+        }
+        if(headPos == sizeAq){
+            toLeft = 1;
+            headPos = (sizeAq - sizeLo + 1);
+            return;
+        }
+        toLeft = 1;
+    }
+   
+   
+    public void mover(){
+        //substituir blocos de virar por virar()
+        if(toLeft == 1){
+            if(headPos == 1){
+                toLeft = 0;
+                headPos = sizeLo;
+                return;
+            }
+            headPos--;
+            return;
+        }
+        if(headPos == sizeAq){
+            toLeft = 1;
+            headPos = (sizeAq - sizeLo + 1);
+            return;
+        }
+        headPos++;
+        return;  
+    }
+   
+    public String apresentar(){
+        String aquario = new String();
+        if(toLeft == 1){
+            for(int scanPos = 1; scanPos <= sizeAq; scanPos++){
+                if(scanPos == headPos){
+                    aquario = aquario + "O";
+                }else if(scanPos > headPos && scanPos - headPos < sizeLo){
+                    aquario = aquario + "@";
+                }else{
+                    aquario = aquario + "#";
+                }
+            }
+        }else{
+        	for(int scanPos = 1; scanPos <= sizeAq; scanPos++){
+                if(scanPos == headPos){
+                    aquario = aquario + "O";
+                }else if(scanPos < headPos && headPos - scanPos < sizeLo){
+                    aquario = aquario + "@";
+                }else{
+                    aquario = aquario + "#";
+                }
+            }
+        }
+        return aquario;
+    }
+   
+}
